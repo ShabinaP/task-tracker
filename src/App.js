ccdc713d95ca2ed2857bc03d6a311a99fd06dc2f
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-10-30 16:00:27 +0100

    created requests to fake API using server JSON

--- src/App.js
+++ src/App.js
@@ -15,16 +15,23 @@ function App() {
     };
     getTasks();
   }, []);
-
+  // fetch tasks
   const fetchTasks = async () => {
     const res = await fetch("http://localhost:5000/tasks");
     const data = await res.json();
     return data;
   };
 
+  // fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
   // add task
   const addTask = async (task) => {
-    const res = await fetch("http://localhost:5000/tasks", {
+    const res = await fetch("http//localhost:5000/tasks", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
@@ -40,10 +47,20 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
   // toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+    const data = await res.json();
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
